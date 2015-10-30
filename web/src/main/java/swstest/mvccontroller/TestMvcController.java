1ebec1495f5a9c600b4327c5e92e862d5925dd39
zhy2002 <hz141@uowmail.edu.au>
2015-10-30 11:50:33 +1100

    tested injection of entity manager.

--- web/src/main/java/swstest/mvccontroller/TestMvcController.java
+++ web/src/main/java/swstest/mvccontroller/TestMvcController.java
@@ -2,10 +2,17 @@ package swstest.mvccontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import swstest.domain.simpleshop.model.Category;
 import swstest.service.simpleshop.CategoryService;
+
+import javax.management.Query;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 /**
  * Created with IntelliJ IDEA.
@@ -24,6 +31,18 @@ public class TestMvcController {
     public String doGet(ModelMap model){
         model.addAttribute("message", "Hello Spring MVC Framework!");
         model.addAttribute("categories", categoryService.findAll());
+        return "test";
+    }
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    @RequestMapping(value = "/test2", method = RequestMethod.GET)
+    public String doGet2(ModelMap model){
+       TypedQuery<Category> query = entityManager.createNamedQuery("Category.findAll", Category.class);
+        model.addAttribute("message", "Hello Spring MVC Framework! - 2");
+        model.addAttribute("categories", query.getResultList());
         return "test";
     }
 }
